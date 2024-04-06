50f565e7eda0e8676cc043166c3b5f58772a89e1
piquerez <corentin.piquerez1@etu.univ-lorraine.fr>
2024-04-06 19:49:47 +0200

    #4 - Boxes

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -2,12 +2,17 @@
 <template>
   
   <!-- HTML -->
-  <Header page_name="Accueil"/> <!--importé dans script--> 
-  <Carousel />
-  <Box title="Actualités du club" description="blabla"/>
-  <p>Contenu du site</p>
-  <!-- Affichage d'une variable: {{ text }}-->
-  <Footer />
+  <header>
+    <Header page_name="Accueil" /> <!--importé dans script--> 
+    <Carousel />
+  </header>
+  <body>
+    <Boxes :contents="BoxesContents"/>
+  </body>
+  <footer>
+    <Footer />
+  </footer>
+
 </template>
 
 
@@ -15,9 +20,30 @@
   // java script
   import Header from '@/components/Header.vue'; // @ = src, définit dans vite.config.js
   import Footer from '@/components/Footer.vue';
-  import Box from '@/components/Box.vue';
+  import Boxes from '@/components/Boxes.vue';
   import Carousel from '@/components/Carousel.vue'
 
+  let BoxesContents = [{
+    title: 'Actualités du club',
+    description: ["Prochain entrainement : lundi 1/04"]
+  }, 
+  {
+    title: 'Prochaines compétitions',
+    description: {
+      type: ["Type de championnat", "Coupe d'Alsace"],
+      date: ["Date", "Samedi 6/04"],
+      place: ["Lieu", "Strasbourg"]
+    }
+  },
+  {
+    title: 'Résultats de la dernière compétition',
+    description: {
+      type: ["Type de championnat ", " Coupe de France"],
+      price: ["Prix", " 2 médailles d'or"],
+      date: ["Date", "Samedi 30/04"],
+      place: ["Lieu", "Lyon"]
+    }
+  }];
 
   //let contenuActus = FileReader();
   //let text = contenuActus.readAsText('text/Accueil_Actus.txt');
@@ -25,5 +51,5 @@
 
 
 <style>
-  @import 'bootstrap/dist/css/bootstrap.css'
+  @import 'bootstrap/dist/css/bootstrap.css';
 </style>
--- FrontEnd/src/components/Box.vue
+++ FrontEnd/src/components/Box.vue
@@ -1,9 +1,20 @@
 <template>
-
-    <h3>{{ title }}</h3>
-    <p>{{ description }}</p>
+    <div class="box">
+        <h3 class="title">{{ content['title'] }}</h3>
+        <div v-for="c in content['description']">
+            <p v-if="c.length == 2">{{ c[0] }} : {{ c[1] }}</p>
+            <p v-else>{{ c }}</p>
+        </div>
+    </div>
 </template>
 
 <script setup>
-    let props = defineProps(['title', 'description']);
+    let props = defineProps({content : Array});
+    console.log(props.content);
+    //
+    //<p v-else>{{ content[c] }}</p>
 </script>
+
+<style lang="scss">
+    @import '../assets/box.scss';
+</style>
